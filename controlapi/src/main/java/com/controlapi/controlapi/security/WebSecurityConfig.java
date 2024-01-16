39ea93bacb43b93de29450005f64cd20f48a1454
Otávio Augusto Ferreira <otavio.ferreira@expocaccer.net>
2024-01-16 08:38:45 -0300

    FEAT: Criando Authorization Server

--- controlapi/src/main/java/com/controlapi/controlapi/security/WebSecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package com.controlapi.controlapi.security;
-
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
-
-public class WebSecurityConfig extends SecurityConfigurerAdapter {
-
-
-}
